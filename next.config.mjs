3066aac174ab9a2707d8798a47d797fd02c91cff
jhleelego@gmail.com <jhleelego@gmail.com>
2024-08-04 23:46:52 +0900

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 const prefix =
-  process.env.NODE_ENV === 'production'
-    ? 'https://jhleelego.github.io/rankingking/'
-    : ''
+  process.env.NODE_ENV === 'production' ? 'https://rankingking.co.kr/' : ''
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
