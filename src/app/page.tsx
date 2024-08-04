34a8aef93c4099e7201217b809299c31de00bdcc
jhleelego@gmail.com <jhleelego@gmail.com>
2024-08-04 21:09:52 +0900

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Item from '@/_components/Item'
 
 const Home = () => {
   return (
-    <main className="flex-1 bg-white p-4 text-black">
+    <main className="flex-1 bg-white p-4 text-black bg-green-100">
       <h2 className="text-xl font-bold ">Home</h2>
+      <span className="text-[100px] bg-white">👑</span>
       <Item />
     </main>
   )
