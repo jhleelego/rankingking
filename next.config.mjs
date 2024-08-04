9900def7944823a640ba46b0e09edbf37049db2e
jhleelego@gmail.com <jhleelego@gmail.com>
2024-08-04 18:23:35 +0900

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
-const isProd = process.env.NODE_ENV === 'production'
+const prefix =
+  process.env.NODE_ENV === 'production'
+    ? 'https://jhleelego.github.io/rankingking/'
+    : ''
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? 'https://jhleelego.github.io/rankingking/' : '',
   reactStrictMode: true,
+  output: 'export',
+  assetPrefix: prefix,
 }
 
 export default nextConfig
