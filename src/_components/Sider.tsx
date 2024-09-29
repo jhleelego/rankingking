255855325f7a8828702e367c281393fc88db657d
jhleelego@gmail.com <jhleelego@gmail.com>
2024-09-30 01:23:40 +0900

    Fix

--- src/_components/Sider.tsx
+++ src/_components/Sider.tsx
@@ -9,6 +9,7 @@ const Sider = ({ onClose }: { onClose: () => void }) => {
   const { data: sampleData, error: sampelError } = useSample(1000)
 
   useEffect(() => {
+    console.log('NEXT_PUBLIC_CP_BASE_URL : ', process.env.NEXT_PUBLIC_CP_BASE_URL)
     console.log('sampleData : ', sampleData)
   }, [sampleData])
 
