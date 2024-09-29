f2f28d1580eb7958f67df58d8ccbb1438103b126
jhleelego@gmail.com <jhleelego@gmail.com>
2024-09-30 00:52:56 +0900

    Fix

--- src/_api/MbrApi.ts
+++ src/_api/MbrApi.ts
@@ -5,4 +5,4 @@ import { useQuery } from '@tanstack/react-query'
 // GET ​/products​/bestcategories​/{categoryId} // 카테고리 별 베스트 상품에 대한 상세 상품 정보를 생성합니다
 
 export const useSample = (categoryId: number) =>
-  useQuery({ queryKey: ['todos'], queryFn: () => getFetchData<ResSuccessMessage<any>, {}>(`products/bestcategories/${categoryId}`) })
+  useQuery({ queryKey: ['todos'], queryFn: () => getFetchData<any, {}>(`products/bestcategories/${categoryId}`) })
