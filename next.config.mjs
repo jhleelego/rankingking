6a64d462db4841aeecf9b3879f08efef3dfec237
jhleelego@gmail.com <jhleelego@gmail.com>
2024-10-09 17:54:32 +0900

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
-const prefix =
-  process.env.NODE_ENV === 'production' ? 'https://rankingking.co.kr/' : ''
+const prefix = process.env.NODE_ENV === 'production' ? 'https://rankingking.co.kr/' : ''
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   assetPrefix: prefix,
 }
 
