e7ca4e7c907786a12e9768f35de7bac542c4b548
jhleelego@gmail.com <jhleelego@gmail.com>
2024-10-15 21:30:12 +0900

    Fix

--- src/util/hmacGenerator.ts
+++ src/util/hmacGenerator.ts
@@ -7,7 +7,6 @@ export const generateHmac = (method: string, url: string, secretKey: string, acc
 
   const datetime = moment.utc().format('YYMMDD[T]HHmmss[Z]')
   const message = datetime + method + path + query
-
   const signature = crypto.createHmac('sha256', secretKey).update(message).digest('hex')
 
   return `CEA algorithm=HmacSHA256, access-key=${accessKey}, signed-date=${datetime}, signature=${signature}`
