6be890f43640c5fd1d126787b10b77ac2e32df45
jhleelego@gmail.com <jhleelego@gmail.com>
2024-10-15 20:13:50 +0900

    Fix

--- src/_components/Sider.tsx
+++ src/_components/Sider.tsx
@@ -9,7 +9,7 @@ const Sider = ({ onClose }: { onClose: () => void }) => {
   const { data: sampleData, error: sampelError } = useSample(1001)
 
   useEffect(() => {
-    console.log('d ef : ', process.env.NEXT_PUBLIC_CP_BASE_URL)
+    console.log('def : ', process.env.NEXT_PUBLIC_CP_BASE_URL)
     console.log('sampleData : ', sampleData)
   }, [sampleData])
 
